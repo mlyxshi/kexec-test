e51e18fe925f593e09e270c4f9c3dc654d0e0db4
mlyxshi <mlyxdev@gmail.com>
2022-12-06 13:25:35 -0800

    test

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, disko }: {
+  outputs = { self, nixpkgs }: {
     nixosConfigurations.test = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
